a59be3446de6e5079ce8907dc2e280245da037ae
Your Name <your-email@example.com>
2025-02-24 19:41:05 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Service from "./pages/Service/Sercice";
@@ -10,7 +10,6 @@ import Message from "./pages/Message/Message";
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -20,7 +19,6 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/message" element={<Message />} />
       </Routes>
-    </Router>
   );
 }
 
